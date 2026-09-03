19c4b7f0cb7d6bae514ba17d44b80b71afb23f46
Sunny Nguyen <nguyen.sunny05@gmail.com>
2026-09-03 15:58:19 +0200

    Mise en place de swagger.json

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { objets } from "./db_objet.js"; 
-import  categoriesRouter from "./routes/categories.js";
-import objetsRouter from "./routes/objets.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger.json" with { type: "json" };
+
+import { objets } from "./db_objet.js"; 
+import categoriesRouter from "./routes/categories.js";
+import objetsRouter from "./routes/objets.js";
 import personnesRouter from "./routes/personnes.js";
 import depotRouter from "./routes/depots.js";
 import statsRouter from "./routes/stats.js";
@@ -11,19 +12,21 @@ import statsRouter from "./routes/stats.js";
 const app = express();
 
 app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/api/categories" , categoriesRouter);
-app.use("/api/objets" , objetsRouter);
-app.use("/api/personnes" , personnesRouter);
-app.use("/api/depots", depotRouter)
-app.use("/api/stats", statsRouter)
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use("/api/categories", categoriesRouter);
+app.use("/api/objets", objetsRouter);
+app.use("/api/personnes", personnesRouter);
+app.use("/api/depots", depotRouter);
+app.use("/api/stats", statsRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ erreur: "Erreur interne du serveur" });
+});
 
 app.listen(3000, () => {
   console.log("Serveur sur http://localhost:3000");
-});
-
-app.use((err, req, res, suite) => {
-  console.error(err);                                   // le détail de l'erreur reste ici
-  res.status(500).json({ erreur: 'Erreur interne du serveur' });  // le client reçoit un message propre
+  console.log("Swagger dispo sur http://localhost:3000/api-docs");
 });
